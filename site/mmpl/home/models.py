0dd91ebbb44dff61cc29274cfa1f1014fff478e7
Ben Elliot <benjiboi214@gmail.com>
2017-03-12 19:23:12 +1100

    Moved top level folder from 'mmpl' to 'site'

---
+++ site/mmpl/home/models.py
@@ -0,0 +1,497 @@
+from __future__ import absolute_import, unicode_literals
+
+from collections import OrderedDict
+
+from django.db import models
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django import forms
+
+from wagtail.wagtailcore.models import Page, Orderable
+from wagtail.wagtailcore.fields import RichTextField, StreamField
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, MultiFieldPanel, \
+    InlinePanel, PageChooserPanel, StreamFieldPanel, FieldRowPanel
+from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
+from wagtail.wagtaildocs.edit_handlers import DocumentChooserPanel
+from wagtail.wagtailsnippets.models import register_snippet
+from wagtail.wagtailforms.models import AbstractEmailForm, AbstractFormField
+from wagtail.wagtailforms.forms import FormBuilder
+
+
+from wagtail.wagtailcore.blocks import TextBlock, StructBlock, StreamBlock, \
+    FieldBlock, CharBlock, RichTextBlock, RawHTMLBlock
+from wagtail.wagtailimages.blocks import ImageChooserBlock
+from wagtail.wagtaildocs.blocks import DocumentChooserBlock
+
+from modelcluster.fields import ParentalKey
+from wagtailmenus.models import MenuPage
+
+
+# Global Streamfield Definitions
+class PullQuoteBlock(StructBlock):
+    quote = TextBlock("quote title")
+    attribution = CharBlock()
+
+    class Meta:
+        icon = "openquote"
+
+
+class ImageFormatChoiceBlock(FieldBlock):
+    field = forms.ChoiceField(choices=(
+        ('left', 'Wrap left'),
+        ('right', 'Wrap right'),
+        ('mid', 'Mid width'),
+        ('full', 'Full width'),
+    ))
+
+
+class HTMLAlignmentChoiceBlock(FieldBlock):
+    field = forms.ChoiceField(choices=(
+        ('normal', 'Normal'), ('full', 'Full width'),
+    ))
+
+
+class ImageBlock(StructBlock):
+    image = ImageChooserBlock()
+    caption = RichTextBlock()
+    alignment = ImageFormatChoiceBlock()
+
+
+class AlignedHTMLBlock(StructBlock):
+    html = RawHTMLBlock()
+    alignment = HTMLAlignmentChoiceBlock()
+
+    class Meta:
+        icon = "code"
+
+
+class HomeStreamBlock(StreamBlock):
+    h2 = CharBlock(icon="title", classname="title")
+    h3 = CharBlock(icon="title", classname="title")
+    h4 = CharBlock(icon="title", classname="title")
+    intro = RichTextBlock(icon="pilcrow")
+    paragraph = RichTextBlock(icon="pilcrow")
+    aligned_image = ImageBlock(label="Aligned image", icon="image")
+    pullquote = PullQuoteBlock()
+    aligned_html = AlignedHTMLBlock(icon="code", label='Raw HTML')
+    document = DocumentChooserBlock(icon="doc-full-inverse")
+
+
+# Link Abstract
+class LinkFields(models.Model):
+    link_text = models.CharField(max_length=40, blank=True)
+    link_external = models.URLField("External link", blank=True)
+    link_page = models.ForeignKey(
+        'wagtailcore.Page',
+        null=True,
+        blank=True,
+        related_name='+'
+    )
+    link_document = models.ForeignKey(
+        'wagtaildocs.Document',
+        null=True,
+        blank=True,
+        related_name='+'
+    )
+
+    @property
+    def link(self):
+        if self.link_page:
+            return self.link_page.url
+        elif self.link_document:
+            return self.link_document.url
+        else:
+            return self.link_external
+
+    panels = [
+        FieldPanel('link_text'),
+        FieldPanel('link_external'),
+        PageChooserPanel('link_page'),
+        DocumentChooserPanel('link_document'),
+    ]
+
+    class Meta:
+        abstract = True
+
+
+# Carousel Abstract
+class CarouselItem(LinkFields):
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+    title = models.CharField(max_length=25)
+    caption = models.CharField(max_length=255, blank=True)
+
+    panels = [
+        ImageChooserPanel('image'),
+        FieldPanel('title'),
+        FieldPanel('caption'),
+        MultiFieldPanel(LinkFields.panels, "Link"),
+    ]
+
+    class Meta:
+        abstract = True
+
+
+# Home Page Classes
+class HeroItem(LinkFields):
+    title = models.CharField(max_length=25, blank=True)
+    page = ParentalKey('home.HomePage', related_name='hero_items')
+    blurb = models.CharField("Blurb", max_length=255, blank=True)
+
+    panels = [
+        FieldPanel('title', classname='full'),
+        FieldPanel('blurb'),
+        MultiFieldPanel(LinkFields.panels, "Link"),
+    ]
+
+
+class HomePageCarouselItem(Orderable, CarouselItem):
+    page = ParentalKey('home.HomePage', related_name='carousel_items')
+
+
+class HomePage(MenuPage):
+    hero_item_title = models.CharField(max_length=25, blank=True)
+    hero_item_blurb = RichTextField(blank=True)
+
+    class Meta:
+        verbose_name = "Homepage"
+
+
+HomePage.content_panels = [
+    FieldPanel('title', classname='full title'),
+    InlinePanel('carousel_items', label="Carousel items"),
+    MultiFieldPanel(
+        [
+            FieldPanel('hero_item_title'),
+            FieldPanel('hero_item_blurb'),
+            InlinePanel(
+                'hero_items',
+                label="Hero Items",
+                max_num=3
+            ),
+        ],
+        heading="Home Page Hero Items",
+        classname="collapsible"
+    ),
+]
+
+
+class BlogPageMediaItem(LinkFields):
+    page = ParentalKey('home.BlogPage', related_name='media_item')
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    panels = [
+        ImageChooserPanel('image'),
+        FieldPanel('link_external')
+    ]
+
+
+class BlogPage(Page):
+    body = StreamField(HomeStreamBlock())
+    date = models.DateField("Post date")
+
+    @property
+    def blog_index(self):
+        # Find closest ancestor which is a blog index
+        return self.get_ancestors().type(BlogIndexPage).last()
+
+    @property
+    def intro(self):
+        intro = []
+        for block in self.body:
+            if block.block_type == 'intro':
+                intro.append(block)
+        return intro
+
+    class Meta:
+        verbose_name = "Blog Page"
+
+BlogPage.content_panels = [
+    FieldPanel('title', classname='full title'),
+    FieldPanel('date'),
+    InlinePanel(
+        'media_item',
+        label="Media Item",
+        max_num=1,
+        help_text="""
+        Always get the URL from the 'src' section of the 'embed' option.
+        Used for populating the blog index media items, and blog page media
+        items. Will try to populate external links first, then image and
+        finally ignore and fill space.
+        """
+    ),
+    StreamFieldPanel('body')
+]
+
+
+class BlogIndexPage(MenuPage):
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    @property
+    def blogs(self):
+        # Get list of live blog pages that are descendants of this page
+        blogs = BlogPage.objects.live().descendant_of(self)
+
+        # Order by most recent date first
+        blogs = blogs.order_by('-date')
+        return blogs
+
+    def get_context(self, request):
+        # Get blogs
+        blogs = self.blogs
+
+        # Pagination
+        page = request.GET.get('page')
+        paginator = Paginator(blogs, 2)  # Show 10 blogs per page
+        try:
+            blogs = paginator.page(page)
+        except PageNotAnInteger:
+            blogs = paginator.page(1)
+        except EmptyPage:
+            blogs = paginator.page(paginator.num_pages)
+
+        # Update template context
+        context = super(BlogIndexPage, self).get_context(request)
+        context['paginator'] = blogs
+        return context
+
+    class Meta:
+        verbose_name = "Blog Index Page"
+
+    content_panels = [
+        FieldPanel('title', classname='full title'),
+        ImageChooserPanel('image'),
+    ]
+
+
+class AboutPageContactItem(LinkFields, Orderable):
+    page = ParentalKey('home.AboutPage', related_name='contact_item')
+    intro = models.CharField(max_length=300, blank=True)
+    icon = models.CharField(max_length=20, verbose_name="Icon Code (fa)")
+
+    panels = [
+        FieldPanel('intro'),
+        FieldPanel('icon'),
+        PageChooserPanel('link_page'),
+    ]
+
+
+class AboutPage(MenuPage):
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+    sub_title = models.CharField(max_length=50, blank=True)
+    body = RichTextField(blank=True)
+    join_item_title = models.CharField(max_length=25, blank=True)
+
+    @property
+    def contact_items(self):
+        contact_items = AboutPageContactItem.objects.filter(page=self)
+        return contact_items
+
+    def get_context(self, request):
+        contact_items = self.contact_items
+        pages = len(contact_items)
+        if pages % 3 == 0:
+            col = 4
+        elif pages % 2 == 0:
+            col = 6
+        elif pages % 1 == 0:
+            col = 6
+        context = super(AboutPage, self).get_context(request)
+        context['contact_items'] = contact_items
+        context['contact_items_col'] = col
+        return context
+
+    class Meta:
+        verbose_name = "About Page"
+
+AboutPage.content_panels = [
+    FieldPanel('title', classname='full title'),
+    ImageChooserPanel('image'),
+    FieldPanel('sub_title'),
+    FieldPanel('body', classname="full"),
+    MultiFieldPanel(
+        [
+            FieldPanel('join_item_title'),
+            InlinePanel(
+                'contact_item',
+                label="Contact Form Item",
+            ),
+        ]
+    )
+]
+
+
+# Abstract for adding CSS and Placeholder attrs to widget
+class CustomFormBuilder(FormBuilder):
+    @property
+    def formfields(self):
+        formfields = OrderedDict()
+
+        for field in self.fields:
+            options = self.get_field_options(field)
+
+            if field.field_type in self.FIELD_TYPES:
+                # import pdb; pdb.set_trace()
+                formfields[field.clean_name] = self.FIELD_TYPES[field.field_type](self, field, options)
+            else:
+                raise Exception("Unrecognised field type: " + field.field_type)
+            if hasattr(formfields[field.clean_name], 'widget'):
+                widget = formfields[field.clean_name].widget
+                large_widgets = ['multiline', 'radio', 'checkboxes']
+                if field.field_type in large_widgets:
+                    widget.large = True
+                else:
+                    widget.large = False
+                cls_attrs = 'form-control input-lg'
+                widget.attrs['class'] = cls_attrs
+                widget.attrs['placeholder'] = field.label
+        return formfields
+
+
+class ContactFormField(AbstractFormField):
+    page = ParentalKey('ContactFormPage', related_name='form_fields')
+
+
+class ContactFormPage(AbstractEmailForm):
+    sub_title = models.CharField(max_length=50, blank=True)
+    intro = RichTextField(blank=True)
+    thank_you_text = RichTextField(blank=True)
+
+    form_builder = CustomFormBuilder
+
+    content_panels = AbstractEmailForm.content_panels + [
+        FieldPanel('sub_title', classname="full"),
+        FieldPanel('intro', classname="full"),
+        InlinePanel('form_fields', label="Form fields"),
+        FieldPanel('thank_you_text', classname="full"),
+        MultiFieldPanel([
+            FieldRowPanel([
+                FieldPanel('from_address', classname="col6"),
+                FieldPanel('to_address', classname="col6"),
+            ]),
+            FieldPanel('subject'),
+        ], "Email"),
+    ]
+
+
+# Social Snippet
+@register_snippet
+class Social(LinkFields):
+    title = models.CharField(max_length=50, verbose_name="Social Site")
+    page = models.ForeignKey(
+        'wagtailcore.Page',
+        related_name='social',
+        null=True,
+        blank=True
+    )
+    icon = models.CharField(max_length=20, verbose_name="Icon Code (fa)")
+
+    panels = [
+        FieldPanel('title', classname='full title'),
+        FieldPanel('link_external'),
+        FieldPanel('icon')
+    ]
+
+    def __unicode__(self):
+        return self.title
+
+
+# Copyight Snippet
+@register_snippet
+class Copyright(models.Model):
+    copyright = models.CharField(
+        max_length=100,
+        verbose_name="Copyright Footer"
+    )
+    page = models.ForeignKey(
+        'wagtailcore.Page',
+        related_name='copyright',
+        null=True,
+        blank=True
+    )
+
+    panels = [
+        FieldPanel('copyright', classname='full title')
+    ]
+
+    def __unicode__(self):
+        return self.copyright
+
+
+# Footer Snippet
+@register_snippet
+class AboutFooter(models.Model):
+    title = models.CharField(
+        max_length=50,
+        verbose_name="Header"
+    )
+    body = models.CharField(
+        max_length=500,
+        verbose_name="Body"
+    )
+    page = models.ForeignKey(
+        'wagtailcore.Page',
+        related_name='footer_about',
+        null=True,
+        blank=True
+    )
+
+    panels = [
+        FieldPanel('title', classname='full title'),
+        FieldPanel('body')
+    ]
+
+    def __unicode__(self):
+        return self.title
+
+
+# Logo Snippet
+@register_snippet
+class Logo(models.Model):
+    description = models.CharField(
+        max_length=100,
+        verbose_name="Description"
+    )
+    page = models.ForeignKey(
+        'wagtailcore.Page',
+        related_name='logo',
+        null=True,
+        blank=True
+    )
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='image'
+    )
+
+    panels = [
+        FieldPanel('description', classname='full title'),
+        ImageChooserPanel('image')
+    ]
+
+    def __unicode__(self):
+        return self.description
---
+++ site/mmpl/home/templatetags/home_tags.py
@@ -0,0 +1,179 @@
+from django import template
+from django.core.exceptions import ObjectDoesNotExist
+
+from home.models import Social, Copyright, AboutFooter, Logo, HeroItem, \
+    Page, BlogPageMediaItem, AboutPageContactItem
+
+register = template.Library()
+
+
+# Site wide get root method
+@register.assignment_tag(takes_context=True)
+def get_site_root(context):
+    # NB this returns a core.Page, not the implementation-specific model used
+    # so object-comparison to self will return false as objects would differ
+    return context['request'].site.root_page
+
+
+# Context test and trace set.
+@register.inclusion_tag('home/tags/test_context.html', takes_context=True)
+def test_context(context):
+    self = context.get('self')  # Should be onstance of home page.
+    import pdb; pdb.set_trace()
+    return {
+        'self': self,
+        'request': context['request'],
+    }
+
+
+# Hero area get include title, blurb and hero items
+@register.inclusion_tag('home/tags/hero_area.html', takes_context=True)
+def hero_area(context):
+    self = context.get('self')  # Should be onstance of home page.
+    hero_items = HeroItem.objects.filter(page=self)
+    return {
+        'self': self,
+        'hero_items': hero_items,
+        'request': context['request'],
+    }
+
+
+# Hero item get specific item model.
+@register.inclusion_tag('home/tags/hero_item.html', takes_context=True)
+def hero_item(context, item):
+    self = context.get('self')  # Should be onstance of home page.
+    return {
+        'blog_index': item.link.specific,
+        'hero_item': item,
+        'request': context['request'],
+    }
+
+
+# Contact Item populate
+@register.inclusion_tag('home/tags/contact_item.html', takes_context=True)
+def contact_item(context, item):
+    self = context.get('self')  # Should be onstance of home page.
+    contact_page = item.link_page
+    return {
+        'contact_page': contact_page,
+        'item': item,
+        'request': context['request'],
+    }
+
+
+# Blog hero area, to display the blog index image and title
+@register.inclusion_tag('home/tags/index_image.html', takes_context=True)
+def index_image(context):
+    self = context.get('self')
+    if self.content_type.model == 'blogindexpage':
+        blog_index = self
+    elif self.content_type.model == 'aboutpage':
+        blog_index = self
+    else:
+        blog_index = self.get_parent().specific
+    return {
+        'blog_index': blog_index,
+        'request': context['request'],
+    }
+
+
+@register.inclusion_tag('home/tags/blog_page_media_item.html', takes_context=True)
+def blog_page_media_item(context):
+    self = context.get('self')
+    media_item = BlogPageMediaItem.objects.get(page=self)
+    return {
+        'media_item': media_item,
+        'request': context['request'],
+    }
+
+
+@register.inclusion_tag('home/tags/blog_index_media_item.html', takes_context=True)
+def blog_index_media_item(context, blog):
+    self = context.get('self')
+    try:
+        media_item = BlogPageMediaItem.objects.get(page=blog)
+    except ObjectDoesNotExist:
+        media_item = None
+    supported_sites = None
+    if media_item:
+        if media_item.link_external:
+            url_split = media_item.link_external.split('.')
+            supported_sites = []
+            if 'youtube' in url_split:
+                supported_sites.append('youtube')
+            elif 'soundcloud' in url_split:
+                supported_sites.append('soundcloud')
+            elif 'vimeo' in url_split:
+                supported_sites.append('vimeo')
+    return {
+        'blog': blog,
+        'media_item': media_item,
+        'supported_sites': supported_sites,
+        'request': context['request'],
+    }
+
+
+# Breadcrumb tag
+@register.inclusion_tag('home/tags/breadcrumbs.html', takes_context=True)
+def breadcrumbs(context):
+    self = context.get('self')
+    if self is None or self.depth <= 2:
+        # When on the home page, displaying breadcrumbs is irrelevant.
+        ancestors = ()
+    else:
+        ancestors = Page.objects.ancestor_of(
+            self, inclusive=True).filter(depth__gt=1)
+    return {
+        'ancestors': ancestors,
+        'request': context['request'],
+    }
+
+
+@register.inclusion_tag('home/tags/pagination.html', takes_context=True)
+def pagination(context):
+    paginator = context['paginator']
+    render_nums = []
+    for num in range(1, paginator.paginator.num_pages + 1):
+        if -1 <= paginator.number - num <= 1:
+            render_nums.append(num)
+    return {
+        'paginator': paginator,
+        'pages': render_nums,
+        'request': context['request'],
+    }
+
+
+# Social Snippet
+@register.inclusion_tag('home/tags/base/social.html', takes_context=True)
+def social(context):
+    return {
+        'socials': Social.objects.select_related('page'),
+        'request': context['request'],
+    }
+
+
+# Copyright Snippet
+@register.inclusion_tag('home/tags/base/copyright.html', takes_context=True)
+def copyright(context):
+    return {
+        'copyright': Copyright.objects.select_related('page'),
+        'request': context['request'],
+    }
+
+
+# Footer Snippet
+@register.inclusion_tag('home/tags/base/footer_about.html', takes_context=True)
+def aboutfooter(context):
+    return {
+        'footer': AboutFooter.objects.select_related('page'),
+        'request': context['request'],
+    }
+
+
+# Logo Snippet
+@register.inclusion_tag('home/tags/base/logo.html', takes_context=True)
+def logo(context):
+    return {
+        'logos': Logo.objects.select_related('page'),
+        'request': context['request'],
+    }
