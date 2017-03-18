d0c7329aa96d2ee9eb760477ace264d28547ee63
Ben Elliot <benjiboi214@gmail.com>
2017-03-19 01:07:36 +1100

    Added development environment option Added line in deploy task to copy uwsgi/%(environment)s.ini to vassals file. Fixed some other bullshit git's complaining about

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,5 @@
 import os
-from fabric.api import cd, env, sudo, settings, task, local, run
+from fabric.api import cd, env, sudo, task
 from fabric.contrib.files import exists
 from fabric.contrib.project import rsync_project
 
@@ -31,25 +31,19 @@ def production():
 
 
 @task
-def setup_server():
-    require_environment()
-
-    # setup media/static directories
-    sudo('mkdir -p %(media)s' % env)
-    with cd(env.media):
-        sudo('mkdir -p {static,media}')
-
-    # setup webapp directory and virtualenv
-    sudo('mkdir -p %(path)s' % env)
-    with cd(env.path):
-        sudo('pip install virtualenv pip --upgrade')
-        if not exists('venv'):
-            sudo('virtualenv -p python venv')
+def development():
+    """Production server settings. Must be first task!"""
+    env.environment = 'development'
+    env.user = 'root'
+    env.app = 'mmpl'
+    env.path = '/var/www/%(app)s/%(environment)s' % env
+    env.media = '/media/%(app)s/%(environment)s' % env
 
 
 @task
 def deploy():
-    """Deploy the application, install requirements, collect static, compress, and migrate"""
+    """Deploy the application, install requirements,
+    collect static, compress, and migrate"""
     require_environment()
 
     with cd(env.path):
@@ -71,7 +65,7 @@ def deploy():
             sudo('mv site rollbacksite')
         sudo('mv deploysite site')
         sudo('chown -R nginx:nginx *')
-        # reload_uwsgi()
+        sudo('cp site/uwsgi/%(environment)s.ini /etc/uwsgi/vassals/%(environment)s.ini' % env)
 
 
 @task
@@ -86,45 +80,8 @@ def rollback():
     with cd(env.path):
         sudo('mv rollbacksite deploysite')
         with cd(os.path.join(env.path, 'deploysite')):
-            sudo('../venv/bin/python manage.py collectstatic --noinput', user='nginx', group='nginx')
-            sudo('../venv/bin/python manage.py compress', user='nginx', group='nginx')
+            sudo('../venv/bin/python manage.py collectstatic --noinput')
+            sudo('../venv/bin/python manage.py compress')
         sudo('mv site rollbacksite')
         sudo('mv deploysite site')
         sudo('chown -R nginx:nginx *')
-        reload_uwsgi()
-
-
-@task
-def reload_nginx():
-    """Reloads or starts the nginx service"""
-    require_environment()
-
-    with settings(warn_only=True):
-        # we check status because calling reload
-        # on a stopped instance returns 0 :(
-        result = sudo('/sbin/service nginx status')
-        if result.return_code == 0:
-            sudo('/sbin/service nginx reload')
-        else:
-            sudo('/sbin/service nginx restart')
-
-
-@task
-def reload_uwsgi():
-    """Reloads or starts the uwsgi emperor service"""
-    require_environment()
-
-    with settings(warn_only=True):
-        result = sudo('/sbin/initctl reload peppermint_%(environment)s' % env)
-        if result.return_code != 0:
-            sudo('/sbin/initctl start peppermint_%(environment)s' % env)
-
-
-@task
-def live_reload_static():
-    """Runs static file compression and collectstatic"""
-    require_environment()
-    with cd(os.path.join(env.path, 'site')):
-        sudo('../venv/bin/python manage.py collectstatic --noinput', user='nginx', group='nginx')
-        sudo('../venv/bin/python manage.py compress', user='nginx', group='nginx')
-    reload_nginx()
