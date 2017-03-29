64802e7b94a015827adf8c2879c0d3d8b2b8647b
Ben Elliot <benjiboi214@gmail.com>
2017-03-30 00:29:36 +1100

    Added email backend settings to dev settings

--- site/mmpl/settings/dev.py
+++ site/mmpl/settings/dev.py
@@ -8,9 +8,18 @@ DEBUG = True
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'jm+*bj5!z(wbj@b23qaha65a$tro150+vx+mobvpg%2!tdzu^j'
 
+# Add here on down to production file.
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'mail.mmpl.org.au'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'thestatistician@mmpl.org.au'
+# EMAIL_HOST_PASSWORD =
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_SUBJECT_PREFIX = '[Wagtail] '
 
+WAGTAILADMIN_NOTIFICATION_FROM_EMAIL = EMAIL_HOST_USER
 
 try:
     from .local import *
