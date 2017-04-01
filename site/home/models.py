377701ddce26c6de15274f169b76e48439dada15
Ben Elliot <benjiboi214@gmail.com>
2017-04-01 23:08:49 +1100

    Changed paginator limit to 10.

--- site/home/models.py
+++ site/home/models.py
@@ -264,7 +264,7 @@ class BlogIndexPage(MenuPage):
 
         # Pagination
         page = request.GET.get('page')
-        paginator = Paginator(blogs, 2)  # Show 10 blogs per page
+        paginator = Paginator(blogs, 10)  # Show 10 blogs per page
         try:
             blogs = paginator.page(page)
         except PageNotAnInteger:
