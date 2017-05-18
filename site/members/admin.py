c8281111aa9f5e1db86c32bed23308b25adfa196
Ben Elliot <benjiboi214@gmail.com>
2017-05-18 12:55:33 +1000

    Adding admin and models changes for safe progress

--- site/members/admin.py
+++ site/members/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Player, Notifications, Venue, Committee
 
-# Register your models here.
+
+class NotificationsInline(admin.StackedInline):
+    model = Notifications
+    extra = 0
+
+
+class PlayerAdmin(admin.ModelAdmin):
+    inlines = [
+        NotificationsInline
+    ]
+    list_display = ('firstname', 'lastname', 'email', 'phone')
+    search_fields = ['firstname', 'lastname']
+    readonly_fields = ['updated']
+
+
+class VenueAdmin(admin.ModelAdmin):
+    list_display = ('name', 'tables', 'contact_name')
+    search_fields = ['name']
+
+class CommitteeAdmin(admin.ModelAdmin):
+    list_display =  ('president', 'start_date', 'end_date')
+
+admin.site.register(Player, PlayerAdmin)
+admin.site.register(Venue, VenueAdmin)
+admin.site.register(Committee, CommitteeAdmin)
