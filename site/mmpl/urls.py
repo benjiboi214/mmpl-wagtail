9287435f2cc8f4cc89001dad2b9a1cffddb3fbdf
Ben Elliot <benjiboi214@gmail.com>
2017-05-30 00:31:20 +1000

    Brought initial migration up to speed, added urls

--- site/mmpl/urls.py
+++ site/mmpl/urls.py
@@ -5,12 +5,15 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 from search import views as search_views
+from members import urls as members_urls
 from wagtail.wagtailadmin import urls as wagtailadmin_urls
 from wagtail.wagtailcore import urls as wagtail_urls
 from wagtail.wagtaildocs import urls as wagtaildocs_urls
 
 urlpatterns = [
     url(r'^django-admin/', include(admin.site.urls)),
+
+    url(r'^members/', include(members_urls)),
 
     url(r'^admin/', include(wagtailadmin_urls)),
     url(r'^documents/', include(wagtaildocs_urls)),
