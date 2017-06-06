8a1078645be3f41418fcb880e02555727ffd53d2
Ben Elliot <benjiboi214@gmail.com>
2017-06-06 20:33:15 +1000

    Added breadcrumbs file I missed, added create view to all verticals

--- site/members/urls.py
+++ site/members/urls.py
@@ -1,15 +1,25 @@
 from django.conf.urls import url
-from members.views import PlayerList, PlayerDetail, VenueList, VenueDetail, \
-    CommitteeList, CommitteeDetail, MemberHome
+from members.views import PlayerListView, PlayerDetailView, PlayerCreateView, \
+    VenueListView, VenueDetailView, VenueCreateView, CommitteeListView, \
+    CommitteeDetailView, CommitteeCreateView, MemberTemplateView
 
 urlpatterns = [
-    url(r'^players/(?P<slug>[\w\-]+)/$', PlayerDetail.as_view(), name='player_detail'),
-    url(r'^players/$', PlayerList.as_view(), name='player_list'),
+    url(r'^players/(?P<slug>[\w\-]+)/$',
+        PlayerDetailView.as_view(),
+        name='player_detail'),
+    url(r'^players/add$', PlayerCreateView.as_view(), name='player_add'),
+    url(r'^players/$', PlayerListView.as_view(), name='player_list'),
     # results view (for search for filtering)
     # update view for editing
-    url(r'^venues/(?P<slug>[\w\-]+)/$', VenueDetail.as_view(), name='venue_detail'),
-    url(r'^venues/$', VenueList.as_view(), name='venue_list'),
-    url(r'^committees/(?P<slug>[\w\-]+)/$', CommitteeDetail.as_view(), name='committee_detail'),
-    url(r'^committees/$', CommitteeList.as_view(), name='committee_list'),
-    url(r'', MemberHome.as_view(), name='members_home')
+    url(r'^venues/(?P<slug>[\w\-]+)/$',
+        VenueDetailView.as_view(),
+        name='venue_detail'),
+    url(r'^venues/add$', VenueCreateView.as_view(), name='venue_add'),
+    url(r'^venues/$', VenueListView.as_view(), name='venue_list'),
+    url(r'^committees/(?P<slug>[\w\-]+)/$',
+        CommitteeDetailView.as_view(),
+        name='committee_detail'),
+    url(r'^committees/add$', CommitteeCreateView.as_view(), name='committee_add'),
+    url(r'^committees/$', CommitteeListView.as_view(), name='committee_list'),
+    url(r'', MemberTemplateView.as_view(), name='members_base')
 ]
--- site/members/views.py
+++ site/members/views.py
@@ -1,5 +1,8 @@
+from django.contrib import messages
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView, TemplateView, \
+    CreateView, UpdateView
 from django.core import serializers
 
 from members.models import Player, Venue, Committee
@@ -7,29 +10,108 @@ from members.models import Player, Venue, Committee
 # Create your views here.
 
 
-class PlayerList(ListView):
+class ActionMixin(object):
+
+    @property
+    def success_msg(self):
+        return NotImplemented
+
+    def form_valid(self, form):
+        messages.info(self.request, self.success_msg)
+        return super(ActionMixin, self).form_valid(form)
+
+
+class PlayerActionMixin(ActionMixin):
+    fields = [
+        'firstname',
+        'lastname',
+        'dob',
+        'email',
+        'phone',
+        'address',
+        'umpire_accreditation',
+        'joined',
+        'media_release',
+        'media_release_date',
+        'vanda_policy',
+        'vanda_policy_date'
+    ]
+
+
+class PlayerCreateView(LoginRequiredMixin, PlayerActionMixin, CreateView):
     model = Player
+    success_msg = 'Player created!'
 
 
-class PlayerDetail(DetailView):
+class PlayerListView(LoginRequiredMixin, ListView):
     model = Player
+    template = "members/player_list.html"
 
 
-class VenueList(ListView):
+class PlayerDetailView(LoginRequiredMixin, DetailView):
+    model = Player
+    template = "members/player_detail.html"
+
+
+# class PlayerUpdateView(LoginRequiredMixin, PlayerActionMixin, UpdateView):
+#     model = Player
+#     success_msg = 'Player updated!'
+#     template = "members/player_update.html"
+
+
+class VenueActionMixin(ActionMixin):
+    fields = [
+        'name',
+        'address',
+        'tables',
+        'phone',
+        'email',
+        'contact_name'
+    ]
+
+
+class VenueCreateView(LoginRequiredMixin, VenueActionMixin, CreateView):
     model = Venue
+    success_msg = 'Venue created!'
 
 
-class VenueDetail(DetailView):
+class VenueListView(LoginRequiredMixin, ListView):
     model = Venue
+    template = "members/venue_list.html"
 
 
-class CommitteeList(ListView):
+class VenueDetailView(LoginRequiredMixin, DetailView):
+    model = Venue
+    template = "members/venue_detail.html"
+
+
+class CommitteeActionMixin(ActionMixin):
+    fields = [
+        'president',
+        'vice_president',
+        'treasurer',
+        'statistician',
+        'secretary',
+        'assistant_secretary',
+        'start_date',
+        'end_date'
+    ]
+
+
+class CommitteeCreateView(LoginRequiredMixin, CommitteeActionMixin, CreateView):
     model = Committee
+    success_msg = 'Committee created!'
 
 
-class CommitteeDetail(DetailView):
+class CommitteeListView(LoginRequiredMixin, ListView):
     model = Committee
+    template = "members/committee_list.html"
 
 
-class MemberHome(TemplateView):
+class CommitteeDetailView(LoginRequiredMixin, DetailView):
+    model = Committee
+    template = "members/committee_detail.html"
+
+
+class MemberTemplateView(LoginRequiredMixin, TemplateView):
     template_name = 'members/base.html'
